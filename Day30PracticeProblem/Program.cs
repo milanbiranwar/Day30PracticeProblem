0b3f90e1ce44912d7fab8ccdc3dd85df52472760
Milan Biranwar <milanbiranwar06@gmail.com>
2022-06-23 19:58:28 +0530

    Program for Ride constructor

--- Day30PracticeProblem/Program.cs
+++ Day30PracticeProblem/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Day30PracticeProblem;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();
